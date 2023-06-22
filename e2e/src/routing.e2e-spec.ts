4b52888daedcdd6b1c2fba13a58fa2ff3928ae89
Anna Blood <ab00721@my.westga.edu>
2023-06-22 15:47:07 -0400

    test navigate to the brackets page

--- e2e/src/routing.e2e-spec.ts
+++ e2e/src/routing.e2e-spec.ts
@@ -3,7 +3,23 @@ import { browser, by, element } from 'protractor';
 describe('Routing', () => {
   it('should navigate to the welcome page', () => {
     browser.get('/');
-    const bracketsTitle = element(by.tagName('h2'));
+    const bracketsTitle = element(by.id('subpageTitle'));
     expect(bracketsTitle.getText()).toContain('Brackets App');
+  });
+
+  it('should navigate to the registration page', () => {
+    browser.get('/');
+    const registrationLink = element(by.id('registration'));
+    registrationLink.click();
+    const registrationTitle = element(by.id('subpageTitle'));
+    expect(registrationTitle.getText()).toContain('Register Players');
+  });
+
+  it('should navigate to the brackets page', () => {
+    browser.get('/');
+    const bracketsLink = element(by.id('brackets'));
+    bracketsLink.click();
+    const bracketsTitle = element(by.id('subpageTitle'));
+    expect(bracketsTitle.getText()).toContain('Brackets');
   });
 });
