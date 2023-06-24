6730c2d9d02b41d3b2886290e33d0aaf05102e91
Anna Blood <ab00721@my.westga.edu>
2023-06-23 21:50:20 -0400

    test Seeding should seed2,4,8 contestants

---
+++ e2e/src/brackets.e2e-spec.ts
@@ -0,0 +1,115 @@
+import { browser, by, element } from 'protractor';
+
+describe('Seeding', () => {
+  beforeEach(() => {
+    browser.get('/registration');
+  });
+
+  it('should seed 2 contestants', () => {
+    const contestants = ['player1', 'player2'];
+    const contestantInputs = element.all(by.css('input[type="text"]'));
+    contestants.forEach((contestant, index) => {
+      contestantInputs.get(index).sendKeys(contestant);
+    });
+    const registerButton = element(by.id('submit'));
+    registerButton.click();
+
+    const bracketsLink = element(by.id('brackets'));
+    bracketsLink.click();
+
+    const matches = element.all(by.css('div[id^="match"]'));
+    expect(matches.count()).toEqual(1);
+
+    const match1 = matches.get(0);
+    const player1match1 = match1.element(by.id('player1match1')); // Replace 'player1match1' with the actual ID for player 1
+    const player2match1 = match1.element(by.id('player2match1')); // Replace 'player2match1' with the actual ID for player 2
+
+    expect(player1match1.isPresent()).toBe(true);
+    expect(player2match1.isPresent()).toBe(true);
+    expect(player1match1.getAttribute('value')).toEqual('player1');
+    expect(player2match1.getAttribute('value')).toEqual('player2');
+  });
+
+  it('should seed 4 contestants', () => {
+    const contestants = ['player1', 'player2', 'player3', 'player4'];
+    const contestantInputs = element.all(by.css('input[type="text"]'));
+    contestants.forEach((contestant, index) => {
+      contestantInputs.get(index).sendKeys(contestant);
+    });
+    const registerButton = element(by.id('submit'));
+    registerButton.click();
+
+    const bracketsLink = element(by.id('brackets'));
+    bracketsLink.click();
+
+    const matches = element.all(by.css('div[id^="match"]'));
+    expect(matches.count()).toEqual(2);
+
+    const match1 = matches.get(0);
+    const player1match1 = match1.element(by.id('player1match1'));
+    const player2match1 = match1.element(by.id('player2match1'));
+
+    const match2 = matches.get(1);
+    const player1match2 = match2.element(by.id('player1match2'));
+    const player2match2 = match2.element(by.id('player2match2'));
+
+    expect(player1match1.isPresent()).toBe(true);
+    expect(player2match1.isPresent()).toBe(true);
+    expect(player1match2.isPresent()).toBe(true);
+    expect(player2match2.isPresent()).toBe(true);
+    expect(player1match1.getAttribute('value')).toEqual('player1');
+    expect(player2match1.getAttribute('value')).toEqual('player2');
+    expect(player1match2.getAttribute('value')).toEqual('player3');
+    expect(player2match2.getAttribute('value')).toEqual('player4');
+  });
+
+  it('should seed 8 contestants', () => {
+    const contestants = ['player1', 'player2', 'player3', 'player4', 'player5', 'player6', 'player7', 'player8'];
+    const contestantInputs = element.all(by.css('input[type="text"]'));
+    contestants.forEach((contestant, index) => {
+      contestantInputs.get(index).sendKeys(contestant);
+    });
+    const registerButton = element(by.id('submit'));
+    registerButton.click();
+
+    const bracketsLink = element(by.id('brackets'));
+    bracketsLink.click();
+
+    const matches = element.all(by.css('div[id^="match"]'));
+    expect(matches.count()).toEqual(4);
+
+    const match1 = matches.get(0);
+    const player1match1 = match1.element(by.id('player1match1'));
+    const player2match1 = match1.element(by.id('player2match1'));
+
+    const match2 = matches.get(1);
+    const player1match2 = match2.element(by.id('player1match2'));
+    const player2match2 = match2.element(by.id('player2match2'));
+
+    const match3 = matches.get(2);
+    const player1match3 = match3.element(by.id('player1match3'));
+    const player2match3 = match3.element(by.id('player2match3'));
+
+    const match4 = matches.get(3);
+    const player1match4 = match4.element(by.id('player1match4'));
+    const player2match4 = match4.element(by.id('player2match4'));
+
+    expect(player1match1.isPresent()).toBe(true);
+    expect(player2match1.isPresent()).toBe(true);
+    expect(player1match2.isPresent()).toBe(true);
+    expect(player2match2.isPresent()).toBe(true);
+    expect(player1match3.isPresent()).toBe(true);
+    expect(player2match3.isPresent()).toBe(true);
+    expect(player1match4.isPresent()).toBe(true);
+    expect(player2match4.isPresent()).toBe(true);
+    expect(player1match1.getAttribute('value')).toEqual('player1');
+    expect(player2match1.getAttribute('value')).toEqual('player2');
+    expect(player1match2.getAttribute('value')).toEqual('player3');
+    expect(player2match2.getAttribute('value')).toEqual('player4');
+    expect(player1match3.getAttribute('value')).toEqual('player5');
+    expect(player2match3.getAttribute('value')).toEqual('player6');
+    expect(player1match4.getAttribute('value')).toEqual('player7');
+    expect(player2match4.getAttribute('value')).toEqual('player8');
+  });
+
+});
