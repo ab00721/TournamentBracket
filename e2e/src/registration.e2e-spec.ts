a074101031f38d781e127d2906ffc939ee8ac412
Anna Blood <ab00721@my.westga.edu>
2023-06-23 21:47:31 -0400

    Test Registration should autofill2,4,8

--- e2e/src/registration.e2e-spec.ts
+++ e2e/src/registration.e2e-spec.ts
@@ -5,6 +5,48 @@ describe('Registration', () => {
     browser.get('/registration');
   });
 
+  it('should autofill 2 contestants', () => {
+    const autofill2 = element(by.id('autoFill2'));
+    autofill2.click()
+
+    const registerButton = element(by.id('submit'));
+    registerButton.click();
+
+    const contestantsList = element(by.id('message'));
+    contestantsList.getText().then((message) => {
+      const contestants = message.split(',');
+      expect(contestants.length).toBe(2);
+    });
+  });
+
+  it('should autofill 4 contestants', () => {
+    const autofill4 = element(by.id('autoFill4'));
+    autofill4.click()
+
+    const registerButton = element(by.id('submit'));
+    registerButton.click();
+
+    const contestantsList = element(by.id('message'));
+    contestantsList.getText().then((message) => {
+      const contestants = message.split(',');
+      expect(contestants.length).toBe(4);
+    });
+  });
+
+  it('should autofill 8 contestants', () => {
+    const autofill8 = element(by.id('autoFill8'));
+    autofill8.click()
+
+    const registerButton = element(by.id('submit'));
+    registerButton.click();
+
+    const contestantsList = element(by.id('message'));
+    contestantsList.getText().then((message) => {
+      const contestants = message.split(',');
+      expect(contestants.length).toBe(8);
+    });
+  });
+
   it('should register 8 contestants', () => {
     const contestants = [ 'player1','player2','player3','player4','player5','player6','player7','player8' ];
     const contestantInputs = element.all(by.css('input[type="text"]'));
@@ -67,7 +109,7 @@ describe('Registration', () => {
 
     const errorMessage = element(by.id('message'));
     errorMessage.getText().then((message) => {
-      expect(message).toContain('Should be 2, 4, or 8 contestants');
+      expect(message).toEqual('Should be 2, 4, or 8 contestants');
     });
   });
 
@@ -82,7 +124,7 @@ describe('Registration', () => {
 
     const errorMessage = element(by.id('message'));
     errorMessage.getText().then((message) => {
-      expect(message).toContain('Should be 2, 4, or 8 contestants');
+      expect(message).toEqual('Should be 2, 4, or 8 contestants');
     });
   });
 
@@ -97,7 +139,7 @@ describe('Registration', () => {
 
     const errorMessage = element(by.id('message'));
     errorMessage.getText().then((message) => {
-      expect(message).toContain('Should be 2, 4, or 8 contestants');
+      expect(message).toEqual('Should be 2, 4, or 8 contestants');
     });
   });
 
@@ -112,7 +154,7 @@ describe('Registration', () => {
 
     const errorMessage = element(by.id('message'));
     errorMessage.getText().then((message) => {
-      expect(message).toContain('Should be 2, 4, or 8 contestants');
+      expect(message).toEqual('Should be 2, 4, or 8 contestants');
     });
   });
 
@@ -127,7 +169,7 @@ describe('Registration', () => {
 
     const errorMessage = element(by.id('message'));
     errorMessage.getText().then((message) => {
-      expect(message).toContain('Should be 2, 4, or 8 contestants');
+      expect(message).toEqual('Should be 2, 4, or 8 contestants');
     });
   });
 
@@ -142,7 +184,7 @@ describe('Registration', () => {
 
     const errorMessage = element(by.id('message'));
     errorMessage.getText().then((message) => {
-      expect(message).toContain('Duplicate player');
+      expect(message).toEqual('Duplicate player');
     });
   });
 
