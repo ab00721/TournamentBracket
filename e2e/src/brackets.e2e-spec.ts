35b5c80eea9834b77057a232901cf559fa6de0fa
Anna Blood <ab00721@my.westga.edu>
2023-06-24 15:25:52 -0400

    test complete 2 rounds, test complete 3 rounds

--- e2e/src/brackets.e2e-spec.ts
+++ e2e/src/brackets.e2e-spec.ts
@@ -115,7 +115,7 @@ describe('Brackets', () => {
     });
   });
 
-  describe('Compete Round', () => {
+  describe('Complete Round', () => {
 
     it('should complete a round with 2 players and player 1 wins', () => {
       const contestants = ['player1', 'player2'];
@@ -161,7 +161,7 @@ describe('Brackets', () => {
       const bracketsLink = element(by.id('brackets'));
       bracketsLink.click();
 
-      const matches = element.all(by.css('div[id^="match"]'));
+      const matches = element.all(by.id('matches'));
       expect(matches.count()).toEqual(1);
 
       const match1 = matches.get(0);
@@ -179,6 +179,115 @@ describe('Brackets', () => {
 
       const round = element(by.id('round'));
       expect(round.getText()).toEqual('Round: 1');
+    });
+
+    it('should complete 2 rounds with 4 players', () => {
+      const contestants = ['player1', 'player2', 'player3', 'player4'];
+      const contestantInputs = element.all(by.css('input[type="text"]'));
+      contestants.forEach((contestant, index) => {
+        contestantInputs.get(index).sendKeys(contestant);
+      });
+      const registerButton = element(by.id('submit'));
+      registerButton.click();
+
+      const bracketsLink = element(by.id('brackets'));
+      bracketsLink.click();
+
+      const matchesR1 = element.all(by.id('matches'));
+      expect(matchesR1.count()).toEqual(2);
+
+      const match1R1 = matchesR1.get(0);
+      const player1match1R1 = match1R1.element(by.id('player1match1'));
+      player1match1R1.click();
+
+      const match2R1 = matchesR1.get(1);
+      const player1match2R1 = match2R1.element(by.id('player1match2'));
+      player1match2R1.click();
+
+      const completeRound = element(by.id('completeRound'));
+      completeRound.click();
+
+      const round = element(by.id('round'));
+      expect(round.getText()).toEqual('Round: 2');
+
+      const matchesR2 = element.all(by.css('div[id^="match"]'));
+      expect(matchesR2.count()).toEqual(1);
+
+      const match1R2 = matchesR2.get(0);
+      const player1match1R2 = match1R2.element(by.id('player1match1'));
+      player1match1R2.click();
+
+      completeRound.click();
+
+      const champion = element(by.id('champion'));
+      expect(champion.getText()).toEqual('Winner: player1');
+    });
+
+    it('should complete 3 rounds with 8 players', () => {
+      const contestants = ['player1', 'player2', 'player3', 'player4', 'player5', 'player6', 'player7', 'player8'];
+      const contestantInputs = element.all(by.css('input[type="text"]'));
+      contestants.forEach((contestant, index) => {
+        contestantInputs.get(index).sendKeys(contestant);
+      });
+      const registerButton = element(by.id('submit'));
+      registerButton.click();
+
+      const bracketsLink = element(by.id('brackets'));
+      bracketsLink.click();
+
+      const round = element(by.id('round'));
+      expect(round.getText()).toEqual('Round: 1');
+
+      const matchesR1 = element.all(by.id('matches'));
+      expect(matchesR1.count()).toEqual(4);
+
+      const match1R1 = matchesR1.get(0);
+      const player1match1R1 = match1R1.element(by.id('player1match1'));
+      player1match1R1.click();
+
+      const match2R1 = matchesR1.get(1);
+      const player1match2R1 = match2R1.element(by.id('player1match2'));
+      player1match2R1.click();
+
+      const match3R1 = matchesR1.get(2);
+      const player1match3R1 = match3R1.element(by.id('player1match3'));
+      player1match3R1.click();
+
+      const match4R1 = matchesR1.get(3);
+      const player1match4R1 = match4R1.element(by.id('player1match4'));
+      player1match4R1.click();
+
+      const completeRound = element(by.id('completeRound'));
+      completeRound.click();
+
+      expect(round.getText()).toEqual('Round: 2');
+
+      const matchesR2 = element.all(by.id('matches'));
+      expect(matchesR2.count()).toEqual(2);
+
+      const match1R2 = matchesR2.get(0);
+      const player1match1R2 = match1R2.element(by.id('player1match1'));
+      player1match1R2.click();
+
+      const match2R2 = matchesR2.get(1);
+      const player1match2R2 = match2R2.element(by.id('player1match2'));
+      player1match2R2.click();
+
+      completeRound.click();
+
+      expect(round.getText()).toEqual('Round: 3');
+
+      const matchesR3 = element.all(by.id('matches'));
+      expect(matchesR3.count()).toEqual(1);
+
+      const match1R3 = matchesR3.get(0);
+      const player1match1R3 = match1R3.element(by.id('player1match1'));
+      player1match1R3.click();
+
+      completeRound.click();
+
+      const champion = element(by.id('champion'));
+      expect(champion.getText()).toEqual('Winner: player1');
     });
   });
 
